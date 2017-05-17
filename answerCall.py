e6e38c46071898b8a405807222e6e6805c2ffe16
Josh Brenne <joshbrenne@gmail.com>
2017-05-16 23:38:01 -0700

    fixed bug

--- answerCall.py
+++ answerCall.py
@@ -8,16 +8,15 @@
 
 def answerCall(caller_code, sameAreaCode, cur_time):
     cur_time = list(cur_time)
-    if (int(cur_time[0]) == 2 and int(cur_time[1]) > 2 == True or
-        int(cur_time[0]) == 0 and int(cur_time[1]) < 7 == True):
+    if ((int(cur_time[0]) == 2 and int(cur_time[1]) > 2) == True or
+        (int(cur_time[0]) == 0 and int(cur_time[1]) < 7) == True):
         return False
+    if caller_code in('F', 'R'):
+        return True
+    elif caller_code in('U') and sameAreaCode == True:
+        return True
     else:
-        if caller_code in('F', 'R'):
-            return True
-        elif caller_code in('U') and sameAreaCode == True:
-            return True
-        else:
-            return False
+        return False
 
 # Make sure it returns a value
 
